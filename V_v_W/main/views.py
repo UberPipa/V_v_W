6953d70059b5230c6b5aa998c5f72f21312c186a
UberPipa <nu.takoe.b@gmail.com>
2023-03-06 12:22:35 +0300

    Сделал нормальную отправку даты, буду добавлять логику в tram

--- V_v_W/main/views.py
+++ V_v_W/main/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 def index(request):
     return render(request, 'main/index.html')
@@ -7,5 +8,14 @@ def index(request):
 def about(request):
     return render(request, 'main/about.html')
 
+
+act_data = {}
 def reports(request):
-    return render(request, 'main/reports.html')
+    global act_data
+    if request.method == 'POST':
+        daterange = request.POST.get('daterange')
+        act_data = {'daterange': daterange}
+    return render(request, 'main/reports.html', {'daterange': daterange})
+
+
+
