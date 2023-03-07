dc29cf2fc58141ca4287bd24fecedb57fbb223aa
UberPipa <nu.takoe.b@gmail.com>
2023-03-07 16:36:17 +0300

    Добавил вывод даты

--- V_v_W/main/views.py
+++ V_v_W/main/views.py
@@ -15,11 +15,11 @@ def about(request):
 def reports(request):
     if request.method == 'POST':
         birthday = request.POST.get('birthday')
-
-        birthday = birthday.split('/')
-        birthday.reverse()
-        birthday = '-'.join(birthday)
-
+        # birthday = birthday.split('/')
+        # birthday.reverse()
+        # birthday = '-'.join(birthday)
         dat = act_data(current_data=birthday, id=0)
         dat.save()
-    return render(request, 'main/reports.html')
+    old_data = act_data.objects.all()
+    return render(request, 'main/reports.html', {'old_data': old_data})
+
