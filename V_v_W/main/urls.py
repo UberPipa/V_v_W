7f23417004dc993b354782fd25337623951328aa
UberPipa <nu.takoe.b@gmail.com>
2023-03-13 14:27:46 +0300

    Сделал 2ю кнопку

--- V_v_W/main/urls.py
+++ V_v_W/main/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
                 path('', views.index, name='index'),
                 path('about', views.about, name='about'),
-                path('reports', views.reports, name='reports')
+                path('reports', views.dataTime, name='reports')
 ]
--- V_v_W/main/views.py
+++ V_v_W/main/views.py
@@ -12,14 +12,33 @@ def about(request):
     return render(request, 'main/about.html')
 
 
-def reports(request):
+# def reports(request):
+#     if request.method == 'POST':
+#         birthday = request.POST.get('birthday')
+#         # birthday = birthday.split('/')
+#         # birthday.reverse()
+#         # birthday = '-'.join(birthday)
+#         dat = act_data(current_data=birthday, id=0)
+#         dat.save()
+#     old_data = act_data.objects.all()
+#     return render(request, 'main/reports.html', {'old_data': old_data})
+
+def dataTime(request):
     if request.method == 'POST':
-        birthday = request.POST.get('birthday')
-        # birthday = birthday.split('/')
-        # birthday.reverse()
-        # birthday = '-'.join(birthday)
-        dat = act_data(current_data=birthday, id=0)
-        dat.save()
+        #print(request.POST.get('inDa'))
+        if 'birthday' in request.POST:  # Для получения даты
+            birthday = request.POST.get('birthday')
+            dat = act_data(current_data=birthday, id=0)
+            dat.save()
+
+        #elif request.POST.get('inDa') == 'run_function_calculate': # Для кнопки расчётов
+        elif 'inDa' in request.POST:  # Для кнопки расчётов
+            inputDate = act_data.objects.all()
+            inputDate = str(inputDate[0])
+            inputDate = inputDate.split('/')
+            inputDate.reverse()
+            inputDate = '-'.join(inputDate)
+            print(inputDate)
+
     old_data = act_data.objects.all()
     return render(request, 'main/reports.html', {'old_data': old_data})
-
